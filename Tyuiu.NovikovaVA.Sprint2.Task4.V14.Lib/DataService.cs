23afe6940af3b5a013e4ee0ab4bc3fb81c9c091b
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-10-09 16:56:47 +0500

    Task4

--- Tyuiu.NovikovaVA.Sprint2.Task4.V14.Lib/DataService.cs
+++ Tyuiu.NovikovaVA.Sprint2.Task4.V14.Lib/DataService.cs
@@ -5,8 +5,9 @@ namespace Tyuiu.NovikovaVA.Sprint2.Task4.V14.Lib
     {
         public double Calculate(double x, double y)
         {
-            double z = (x*3)<(Math.Sqrt(y)+20)?Math.Pow((2+(1/(Math.Pow(x,2)))),y) : ((Math.Pow(y,2) - Math.Cos(Math.Pow(x,2))+10)/(Math.Pow(x, 2) - Math.Sin(Math.Pow(y, 2)) + 12));
+            double z = (x*3)<(Math.Sqrt(y)+20)?Math.Pow((2+(1/(Math.Pow(x,2)))),y) : ((Math.Pow(y,2)-(Math.Pow(Math.Cos(x), 2))) + 10) / (Math.Pow(x,2)-(Math.Pow(Math.Sin(y), 2))+ 12);
             return Math.Round(z,3);
         }
     }
 }
+
