486fdfbd0b4e5fcbae9f6c3c6fcf1e5411570069
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-10-09 17:55:43 +0500

    Task5

---
+++ Tyuiu.NovikovaVA.Sprint2.Task5.V15.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.NovikovaVA.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            string res = "";
+            switch (k % 7)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 7:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    throw new Exception("Вы ввели не число");
+
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.NovikovaVA.Sprint2.Task5.V15/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.NovikovaVA.Sprint2.Task5.V15.Lib;
+namespace Tyuiu.NovikovaVA.Sprint2.Task5.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int k;
+            k = Convert.ToInt32(Console.ReadLine());
+            DataService ds = new DataService();
+            k = Convert.ToInt32(Console.ReadLine());
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("K = " + k);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var res = ds.FindDayName(k);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
